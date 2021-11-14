6a159495212b8be4b579737e66d02dac037d2cce
Meg <meganriley806@gmail.com>
2021-11-14 16:47:50 -0600

    update contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ export default function Contact() {
       fetch("/", {
         method: "POST",
         headers: { "Content-Type": "application/x-www-form-urlencoded" },
-        body: encode({ "form-name": "Contact", name, email, message }),
+        body: encode({ "form-name": "contact", name, email, message }),
       })
         .then(() => alert("Message sent!"))
         .catch((error) => alert(error));
@@ -65,9 +65,10 @@ export default function Contact() {
             <form
               netlify
               name="contact"
+              onSubmit={handleSubmit}
               className="lg:w-1/3 md:w-1/2 flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
               <h2 className="text-white sm:text-4xl text-3xl mb-1 font-medium title-font">
-                Hire Me
+                Let's Chat
               </h2>
               <p className="leading-relaxed mb-5">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Illum

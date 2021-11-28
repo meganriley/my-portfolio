6692f3b3a06ab615b4092aa885e3a032c494d270
Meg <meganriley806@gmail.com>
2021-11-28 09:09:06 -0600

    working functional blog

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -51,7 +51,7 @@ export default function Contact() {
                   <h2 className="title-font font-semibold text-white tracking-widest text-xs">
                     EMAIL
                   </h2>
-                  <a className="text-indigo-400 leading-relaxed">
+                  <a href="mailto:meganriley806@gmail.com" className="text-indigo-400 leading-relaxed">
                     meganriley806@gmail.com
                   </a>
                   <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
